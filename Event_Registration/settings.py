d2750ef37fe0f0cd05d6f6879b88019e6f560079
Hasin-Arman <hasinarman6@gmail.com>
2023-11-29 22:35:11 +0600

    remove cors

--- Event_Registration/settings.py
+++ Event_Registration/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'accounts',
     'events',
     'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'Event_Registration.urls'
